e736d17f005808a12b4ed3caa2ac607faa7a4b31
hanjihae <wlgo32@naver.com>
2024-05-29 17:08:56 +0900

    baekjoon

---
+++ src/Day1/bk2675.java
@@ -0,0 +1,24 @@
+package Day1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bk2675 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int R = Integer.parseInt(br.readLine());
+        for (int i = 0; i < R; i++) {
+            String[] S = br.readLine().split(" ");
+            int num = Integer.parseInt(S[0]);
+            String str = S[1];
+            StringBuilder ans = new StringBuilder();
+            for (int j=0; j < str.length(); j++) {
+                char c = str.charAt(j);
+                for (int k=0; k < num; k++) {
+                    ans.append(c);
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}
