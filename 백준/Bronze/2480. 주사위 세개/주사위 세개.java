9314d80eec3967eeb3f87eb31614961ca7fdd428
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-05-31 20:35:49 +0900

    [Bronze IV] Title: 주사위 세개, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(input[2]);
+
+        int money = 0;
+        int num = 0;
+        if ( a == b && b == c && a == c) {
+            money = 10000 + a*1000;
+        } else if ( a == b  || b == c || a == c) {
+            num = b == c ? b : a;
+            money = 1000 + num*100;
+        } else {
+            num = Math.max(a, Math.max(b, c));
+            money = num*100;
+        }
+        System.out.println(money);
+    }
+}
