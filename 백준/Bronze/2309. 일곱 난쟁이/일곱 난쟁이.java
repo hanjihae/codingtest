37c2af8567208a3200e89a2df5555733b6c077c7
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-06-07 12:15:41 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] gnome = new int[9];
+        int sum = 0;
+        for (int i=0; i < 9; i++) {
+            gnome[i] = Integer.parseInt(br.readLine());
+            sum += gnome[i];    // 아홉 난쟁이 키의 합
+        }
+        for (int i=0; i < 8; i++) {
+            for (int j=i+1; j < 9; j++) {
+                if (sum - gnome[i] - gnome[j] == 100) { // 모든 난쟁이 키의 합에서 두 난쟁이의 키를 뺐을 때 100인 경우
+                    gnome[i] = 0;
+                    gnome[j] = 0;
+                    Arrays.sort(gnome); // 오름차순 정렬
+                    // 오름차순 정렬시 0 처리된 두 난쟁이의 인덱스는 각가 0, 1이므로 gnome[2]~gnome[8] 출력
+                    for (int k=2; k < 9; k++) {
+                        System.out.println(gnome[k]);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
