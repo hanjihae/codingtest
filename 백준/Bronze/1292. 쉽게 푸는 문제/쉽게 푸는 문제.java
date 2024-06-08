ee04c13aaa149582d091964bc68133a656f594c2
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-06-08 10:18:27 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -0,0 +1,24 @@
+// fail
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int cnt = 0;    // 반복 횟수 체크
+        int ans = 0;    // 범위 내 합
+        for (int i = 1; i <= b; i++) {
+            for (int j = 1; j <= i; j++) {
+                cnt++;
+                // 반복 횟수가 a - b 사이 일 때의 i의 누적합
+                if (a <= cnt && cnt <= b) ans += i;
+                if (cnt > b) break;
+            }
+        }
+        System.out.println(ans);
+    }
+}
