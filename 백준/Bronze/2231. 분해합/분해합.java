447575e07b1e420ab24056a412259213f6ea1390
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-07-29 21:46:40 +0900

    [Bronze II] Title: 분해합, Time: 140 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -7,15 +7,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int ans = 0;    // 생성자가 없는 경우 0 출력
-        for (int i = 1; i < n; i++) {
-            String str = String.valueOf(i);
+        for (int i = 1; i < n; i++) { // n의 생성자 찾기
+            int num = i;
             int sum = 0;
-            for (int j = 0; j < str.length(); j++) {
-                sum += str.charAt(j) - '0';
+            while (num != 0) {  // i의 각 자릿수의 합 구하기
+                sum += num % 10;
+                num /= 10;
             }
-            if (i + sum == n) {
+            if (i + sum == n) { // i + i의 각 자릿수의 합 == n일 때 n의 생성자는 i
                 ans = i;
-                break;
+                break;  // 1부터 시작했으므로 조건에 맞는 수가 있다면 그 수가 가장 작은 생성자
             }
         }
         System.out.println(ans);
