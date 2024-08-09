6a11b39ba61c30bc0782a9b52275ac2f52ddeb9d
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-08-09 21:34:33 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 708 ms, Memory: 56312 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] matrix = new int[n][2];
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            matrix[i][0] = Integer.parseInt(st.nextToken());
+            matrix[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(matrix, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < matrix.length; i++) {
+            sb.append(matrix[i][0] + " " + matrix[i][1] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
