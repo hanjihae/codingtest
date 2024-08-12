498c9c6b4abc31cf4047103ca126b1be99052a63
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-08-12 21:51:52 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 132 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        List<Integer> list = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        int index = 0;
+        while(list.size() > 0) {
+            index = (index + k - 1) % list.size();  // 현재 인덱스 + k-1만큼 이동 % 남은 인원
+            sb.append(list.remove(index));  // 
+            if (list.size() > 0) sb.append(", ");
+        }
+        sb.append(">");
+        System.out.println(sb);
+    }
+}
