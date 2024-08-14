8f80aced0bf4da275298ca7648838eb6027b0d58
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-08-14 21:39:48 +0900

    [Silver IV] Title: 수 찾기, Time: 624 ms, Memory: 46720 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,24 +1,45 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        HashSet<Integer> set = new HashSet<>();
-        StringTokenizer a = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arrA = new int[n];
         for (int i = 0; i < n; i++) {
-            set.add(Integer.parseInt(a.nextToken()));
+            arrA[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arrA);
         int m = Integer.parseInt(br.readLine());
-        StringTokenizer b = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            if (set.contains(Integer.parseInt(b.nextToken()))) {
-                System.out.println(1);
+            int num = Integer.parseInt(st.nextToken());
+            int yn = findNumber(arrA, num) ? 1: 0;
+            sb.append(yn + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static boolean findNumber(int[] arr, int num) {
+        int start = 0;
+        int end = arr.length-1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == num) {
+                return true;
             } else {
-                System.out.println(0);
+                if (arr[mid] > num) {
+                    end = mid - 1;
+                } else if (arr[mid] < num){
+                    start = mid + 1;
+                }
             }
         }
+        return false;
     }
 }
