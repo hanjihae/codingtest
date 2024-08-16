64c525f8bda60f4d823066e37081f1c76fc92778
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-08-16 21:27:47 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -6,19 +6,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int bongji = 0;
-        while(n > 0) {
-            if (n % 5 == 0) {   // 5kg짜리로 커버되면
-                bongji += n/5;
+        int ans = 0;
+        while (n > 0) {
+            if (n % 5 == 0) {   // 5로 나누어지는 경우
+                ans += n / 5;
                 break;
-            } else {   // 아닐 경우 3kg거나 나눠 담지 못하는 경우
+            } else {    // 나누어지지 않거나 3으로 나누어지는 경우
                 n -= 3;
-                bongji++;
+                ans++;
             }
-            if (n < 0) {    // n킬로그램으로 만들 수 없는 경우 -1 출력
-                bongji = -1;
-            }
+            // n이 0보다 작다면 -1 출력
+            if (n < 0) ans = -1;
         }
-        System.out.println(bongji);
+        System.out.println(ans);
     }
 }
