b96b9a66fe10e0ff48826eda00deef80f8d2ec8a
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-08-16 22:25:49 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45452 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+        int ans = 0;
+        while(!queue.isEmpty()) {
+            ans = queue.poll();
+            if (!queue.isEmpty()) {
+                queue.offer(queue.poll());
+            }
+        }
+        System.out.println(ans);
+    }
+}
