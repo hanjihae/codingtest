62d8a4b510ea96b0172f3f9fbd0b45f17f11b6a4
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-08-23 21:54:45 +0900

    [Silver III] Title: 프린터 큐, Time: 136 ms, Memory: 15000 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,43 +1,55 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+class Doc {
+    int index;  // 문서 인덱스
+    int priority;   // 문서 중요도
+    public Doc(int index, int priority) {
+        this.index = index;
+        this.priority = priority;
+    }
+}
+
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());    // 테스트케이스 수
+        int t = Integer.parseInt(br.readLine());
         for (int i = 0; i < t; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());   // 문서의 개수
-            int m = Integer.parseInt(st.nextToken());   // 몇번째로 인쇄되었는지 궁금한 문서의 개수
-            LinkedList<int[]> queue = new LinkedList<>();   // 문서의 위치와 중요도 저장 큐
-            StringTokenizer doc = new StringTokenizer(br.readLine());
-            // 각 문서를 큐에 위치와 중요도를 배열로 저장
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            Queue<Doc> queue = new LinkedList<>();
             for (int j = 0; j < n; j++) {
-                queue.offer(new int[] {j, Integer.parseInt(doc.nextToken())});
+                int num = Integer.parseInt(st.nextToken());
+                queue.add(new Doc(j, num));
             }
-            int cnt = 0;    // 인쇄 순서
-            while(!queue.isEmpty()) {
-                int[] first = queue.poll(); // 첫번째 문서 꺼내기
-                boolean max = true; // 현재 문서의 중요도가 가장 높은지
-                for (int j = 0; j < queue.size(); j++) {
-                    if (first[1] < queue.get(j)[1]) {   // 현재 문서보다 중요도가 높은 문서가 있는 경우
-                        queue.offer(first); // 현재 문서를 큐의 마지막에 추가
-                        // 현재 문서보다 중요도가 높은 문서를 순서대로 앞에 오도록
-                        for (int k = 0; k < j; k++) {
-                            queue.offer(queue.poll());
-                        }
-                        max = false;    // 현재 문서는 가장 중요한 문서가 아님
-                        break;  // 비교 종료
+            int order = 0;  // 인쇄 순서
+            while (!queue.isEmpty()) {
+                Doc cur = queue.poll();
+                boolean hasHigherPriority = false;
+                for (Doc d : queue) {
+                    // 문서 중 현재 문서의 중요도보다 높은 게 있는지 확인
+                    if (d.priority > cur.priority) {
+                        hasHigherPriority = true;
+                        break;
                     }
                 }
-                if (!max) continue; // 현재 문서가 가장 중요한 문서가 아닌 경우 다시 반복
-                // 현재 문서가 가장 중요한 문서인 경우
-                cnt++;  // 인쇄 순서
-                if(first[0] == m) break;    // 현재 문서가 궁금한 문서인 경우 종료
+                // 현재 문서보다 중요도 높은 게 있다면 큐에 현재 문서 넣기
+                if (hasHigherPriority) {
+                    queue.add(cur);
+                } else {    // 현재 문서가 가장 높은 중요도를 가진다면
+                    order++;    // 순서 카운트
+                    if (cur.index == m) {   // 현재 문서의 인덱스가 m과 같다면 order 출력
+                        System.out.println(order);
+                        break;
+                    }
+                }
             }
-            System.out.println(cnt);
         }
     }
 }
