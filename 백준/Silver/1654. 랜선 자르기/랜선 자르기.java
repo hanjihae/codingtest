ebb4c980ec7a92129e600b4baf32c57e53b46582
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-08-26 21:38:10 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 25032 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,34 +1,36 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        long[] arr = new long[K];
-        long max = 0;
-        for (int i = 0; i < K; i++) {
+        int k = Integer.parseInt(st.nextToken());   // k <= n
+        int n = Integer.parseInt(st.nextToken());
+        long[] arr = new long[n];
+        long max = Long.MIN_VALUE;
+        for (int i = 0; i < k; i++) {
             arr[i] = Long.parseLong(br.readLine());
             max = Math.max(max, arr[i]);
         }
         long start = 1;
         long end = max;
         while(start <= end) {
-            long mid = (start+end)/2;
-            long sum = 0;
-            for (int i = 0; i < K; i++) {   // 잘라진 랜선 개수 합
-                sum += arr[i]/mid;
+            long mid = (start + end) / 2;
+            int cnt = 0;    // mid 길이로 자른 랜선 개수
+            for (int i=0; i < k; i++) {
+                cnt += arr[i]/mid;
             }
-            // long sum = Arrays.stream(arr).map(e -> e / mid).sum();
-            if (sum >= N) {
+            if (cnt >= n) {
+                // mid 길이로 n개 이상의 랜선을 만들 수 있다면 더 긴 랜선 길이를 찾기 위해
                 start = mid + 1;
-            } else {
+            } else {    // mid 길이로 n개 이상 랜선을 만들 수 없을 때
                 end = mid - 1;
             }
         }
+        // cnt >= n을 만족하는 가장 긴 랜선 길이
         System.out.println(end);
     }
 }
