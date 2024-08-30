a71f0d5571b9935ae4f2340fb8ee6fd8a8a7137e
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-08-30 21:20:38 +0900

    [Silver IV] Title: 듣보잡, Time: 292 ms, Memory: 26212 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,26 +1,27 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        HashSet<String> nList = new HashSet<>();
-        TreeSet<String> list = new TreeSet<>();
-        for (int i = 0; i < N; i++) {
-            nList.add(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Set<String> heardX = new HashSet<>();
+        for (int i=0; i < n; i++) { // 듣도 못한 사람
+            heardX.add(br.readLine());
         }
-        for (int i = 0; i < M; i++) {
-            String str = br.readLine();
-            if (nList.contains(str)) {
-                list.add(str);
+        Set<String> xx = new TreeSet<>();   // 사전순 정렬
+        for (int i=0; i < m; i++) { // 보도 못한 사람
+            String input = br.readLine();
+            if (heardX.contains(input)) {   // 듣도 못한 사람과 동일하면
+                xx.add(input);
             }
         }
-        System.out.println(list.size());
-        for (String s : list) {
+        System.out.println(xx.size());
+        for (String s : xx) {
             System.out.println(s);
         }
     }
