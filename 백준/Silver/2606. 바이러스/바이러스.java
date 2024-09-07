bd33017ee2320fcf8d3235914e9a082f251d9d98
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-09-07 21:38:31 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,47 +1,36 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    static boolean[][] graph;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int vertex = Integer.parseInt(br.readLine());
-        int edge = Integer.parseInt(br.readLine());
-        int[][] graph = new int[vertex+1][vertex+1];
-        boolean[] visited = new boolean[vertex+1];
-        for(int i = 0; i < edge; i++) {
+        int n = Integer.parseInt(br.readLine()); // 컴퓨터의 수
+        int m = Integer.parseInt(br.readLine()); // 네트워크 상에서 연결된 컴퓨터 쌍의 수
+        graph = new boolean[n + 1][n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            graph[x][y] = 1;
-            graph[y][x] = 1;
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u][v] = true;
+            graph[v][u] = true; // 양방향 연결
         }
-        dfs(graph, visited, 1);
-        int ans = 0;
-        for(int i=0; i < visited.length; i++) {
-            if(visited[i]) {
-                if (i == 1) continue;   // 1번 컴퓨터 제외
-                ans++;
-            }
-        }
-        System.out.println(ans);
+        int infectedCount = dfs(1) - 1; // 1번 컴퓨터 제외
+        System.out.println(infectedCount);
     }
 
-    private static void dfs(int[][] graph, boolean[] visited, int start) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(start);
-        visited[start] = true;
-        while(!stack.isEmpty()) {
-            int vertex = stack.pop();
-            for (int i=1; i < graph.length; i++) {
-                // 인접행렬을 봤을 때 연결된 정점인지
-                // 방문 리스트를 봤을 때 방문한 적 없는 정점인지
-                if (graph[vertex][i] == 1 && visited[i] == false) {
-                    visited[i] = true;
-                    stack.push(i);
-                }
+    public static int dfs(int node) {
+        visited[node] = true;
+        int count = 1;
+        for (int i = 1; i < graph.length; i++) {
+            if (graph[node][i] && !visited[i]) {
+                count += dfs(i);
             }
         }
+        return count;
     }
 }
