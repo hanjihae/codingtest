79214c1c701dab46c1553f6383c46d396e5af9b8
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-09-27 21:37:06 +0900

    [Silver II] Title: 좌표 압축, Time: 1780 ms, Memory: 265172 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,30 +1,30 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N]; // N개의 좌표
-        int[] sorted = new int[N];  // 를 오름차순 정렬
-        Map<Integer, Integer> map = new HashMap<>();
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        int[] arr = new int[n];
+        for (int i=0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            sorted[i] = arr[i];
         }
-        Arrays.sort(sorted);    // 정렬된 arr
-        int rank = 0;
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+        Map<Integer, Integer> map = new HashMap<>();
+        int order = 0;
         for (int value : sorted) {
-            if (!map.containsKey(value)) {  // map에 좌표가 포함되지 않은 경우
-                map.put(value, rank);   // 좌표, 순위 map에 저장   
-                rank++;
+            if (!map.containsKey(value)) {  // 해당 숫자를 갖고 있지 않을 때
+                map.put(value, order++);    // 랭크 저장
             }
         }
         StringBuilder sb = new StringBuilder();
-        for (int value : arr) {
-            sb.append(map.get(value)).append(' ');  // map에서 좌표 == value 검색해 rank 출력
+        for (int num : arr) {
+            // 원래 배열에 저장되어 있는 순서대로 map에 저장된 랭크 호출
+            sb.append(map.get(num)).append(" ");
         }
         System.out.println(sb);
     }
