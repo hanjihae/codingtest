c03a071e2107727bc39114f64f85971aea957e01
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-10-02 21:38:31 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 18824 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,48 +1,48 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int n, k;
-    static int[] graph = new int[100001];   // 각 위치까지의 이동 횟수를 저장
-    public static void bfs(int n) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(n);   // 시작 위치를 큐에 저장
-        graph[n] = 1;   // 시작 위치 방문 표시
-        while(!queue.isEmpty()) {
-            int now = queue.poll(); // 큐에서 현재 위치 꺼냄
-            for (int i=0; i < 3; i++) { // +1, -1, *2 세가지 이동 검사
-                int next;
-                if (i==0) {
-                    next = now + 1;
-                } else if (i==1) {
-                    next = now - 1;
-                } else {
-                    next = now*2;
-                }
-                if (next == k) {    // 동생의 위치와 같을 때
-                    System.out.println(graph[now]); // 현재 위치까지의 이동 횟수 출력
-                    return;
-                }
-                // 이동할 위치가 범위 안에 있고, 아직 방문하지 않은 위치일 때
-                if (next >= 0 && next < graph.length && graph[next] == 0) {
-                    queue.add(next);    // 큐에 추가
-                    graph[next] = graph[now] + 1;   // 이동 횟수 기록
-                }
-            }
-        }
-    }
-    public static void main(String[] args) throws Exception {
+    public static int n, k;
+    public static int[] arr = new int[100001];
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());   //  수빈이가 있는 위치
-        k = Integer.parseInt(st.nextToken());   //  동생이 있는 위치
-        if (n == k) {   // 수빈이와 동생의 위치가 같다면 0 출력
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        if (n == k) {
             System.out.println(0);
         } else {
             bfs(n);
+        }
+    }
+
+    public static void bfs(int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(n);
+        arr[n] = 1;
+        while(!queue.isEmpty()) {
+            int x = queue.poll();
+            for (int i=0; i < 3; i++) {
+                int next;
+                if (i == 0) {   // 세가지 이동 검사
+                    next = x - 1;
+                } else if (i == 1) {
+                    next = x + 1;
+                } else {
+                    next = x * 2;
+                }
+                if (next == k) {
+                    System.out.println(arr[x]);
+                    return;
+                }
+                // 범위 안이고 방문하지 않은 위치일 때
+                if (next >= 0 && next < arr.length && arr[next] == 0) {
+                    queue.add(next);
+                    arr[next] = arr[x] + 1; // 이동 횟수 기록
+                }
+            }
         }
     }
 }
