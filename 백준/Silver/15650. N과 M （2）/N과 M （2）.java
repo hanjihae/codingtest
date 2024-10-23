c0265ce0a11f474f2966d309ecd99c3afd90dad9
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-10-23 21:30:06 +0900

    [Silver III] Title: N과 M (2), Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,33 +1,36 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Main {
-
-    public static int N, M;
-    public static int[] arr;
-    public static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws Exception {
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[M];
-        dfs(1,0);
-        System.out.println(sb.toString());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        dfs(1, 0);
+        System.out.println(sb);
     }
 
-    public static void dfs (int start, int depth) {
-        if (depth == M) {
-            for (int a : arr) {
+    public static void dfs(int start, int index) {
+        if(index == m) {    // arr가 다 채워지면 요소 sb에 append해서 종료
+            for(int a : arr) {
                 sb.append(a).append(" ");
             }
             sb.append("\n");
             return;
         }
-        for (int i=start; i <= N; i++) {
-            arr[depth] = i;
-            dfs(i+1, depth+1);
+        for(int i=start; i <= n; i++) { // start: 1 ~ n-1
+            arr[index] = i;
+            dfs(i+1, index+1);
         }
     }
 }
