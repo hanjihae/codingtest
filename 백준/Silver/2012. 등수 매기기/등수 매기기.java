530d4c200f6f34cec9d01800e6b97bf2ea482cdc
hanjihae <86669962+hanjihae@users.noreply.github.com>
2024-11-29 21:33:33 +0900

    [Silver III] Title: 등수 매기기, Time: 680 ms, Memory: 52340 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.java
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -7,15 +7,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] rank = new int[n];
-        for (int i = 0; i < n; i++) {
-            rank[i] = Integer.parseInt(br.readLine());  // 각 사람의 예상등수
+        int[] rank = new int[n + 1];
+        for (int i=1; i <= n; i++) {
+            rank[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(rank);  // 불만도의 합을 최소화하기 위해 예상등수 오름차순 정렬
-        long ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans += Math.abs(rank[i] - (i+1));
+        Arrays.sort(rank);  // 불만도의 합을 최소화하기 위해 오름차순 정렬
+        long cnt = 0;
+        for (int i=1; i <= n; i++) {
+            cnt += Math.abs(rank[i] - i);   // 예상 등수와 실제 등수 차
         }
-        System.out.println(ans);
+        System.out.println(cnt);
     }
 }
