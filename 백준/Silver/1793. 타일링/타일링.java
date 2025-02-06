552684a754492fe462f6f7335eab3472d0899722
hanjihae <86669962+hanjihae@users.noreply.github.com>
2025-02-06 15:48:48 +0900

    [Silver II] Title: 타일링, Time: 116 ms, Memory: 14588 KB -BaekjoonHub

---
+++ 백준/Silver/1793. 타일링/타일링.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger[] dp = new BigInteger[251];
+        dp[0] = BigInteger.ONE; // dp[0] = 1
+        dp[1] = BigInteger.ONE; // dp[1] = 1
+        for (int i = 2; i <= 250; i++) {
+            // dp[i] = dp[i - 1] + dp[i - 2]
+            dp[i] = dp[i - 1].add(dp[i - 2].multiply(BigInteger.TWO));
+        }
+        StringBuilder sb = new StringBuilder();
+        String input;
+        while((input = br.readLine()) != null) {
+            int n = Integer.parseInt(input);
+            sb.append(dp[n]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
