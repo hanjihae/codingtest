03814283a22576c50fecad48fcd6638f9dceaa24
hanjihae <86669962+hanjihae@users.noreply.github.com>
2025-02-14 10:53:30 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -10,15 +10,15 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         if (n == 1 || n == 2) {
             System.out.println(1);
-        } else {
-            long[] dp = new long[n + 1];
-            dp[1] = 1;  // 1
-            dp[2] = 1;    // 10
-//            dp[3] = 2;    // 100, 101
-            for (int i = 3; i <= n; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2];
-            }
-            System.out.println(dp[n]);
+            return;
         }
+        long[] dp = new long[n + 1];
+        dp[1] = 1;  // 1
+        dp[2] = 1;    // 10
+//            dp[3] = 2;    // 100, 101
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[n]);
     }
 }
