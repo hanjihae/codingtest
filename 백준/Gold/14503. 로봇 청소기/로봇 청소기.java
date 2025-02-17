868f835170b3c99f0ee51b852d1a1c75421e5ac1
hanjihae <86669962+hanjihae@users.noreply.github.com>
2025-02-17 10:57:10 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -15,12 +15,10 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         st = new StringTokenizer(br.readLine());
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
         int d = Integer.parseInt(st.nextToken());   // 0=북, 1=동, 2=남, 3=서
-
         room = new int[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -28,7 +26,6 @@ public class Main {
                 room[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         clean(r, c, d);
         System.out.println(cnt);
     }
@@ -36,32 +33,27 @@ public class Main {
     public static void clean(int x, int y, int dir) {
         // 1. 현재 위치 청소
         if (room[x][y] == 0) { // 청소가 안 된 경우만 청소
-            room[x][y] = -1; // 청소 완료 표시
+            room[x][y] = -1; // 청소 완료
             cnt++;
         }
-
         // 2. 주변 4칸 확인
         for (int i = 0; i < 4; i++) {
             dir = (dir + 3) % 4; // 반시계 방향 회전
             int nx = x + dx[dir];
             int ny = y + dy[dir];
-
             if (nx >= 0 && ny >= 0 && nx < n && ny < m && room[nx][ny] == 0) {
                 clean(nx, ny, dir);
                 return; // 이동 후 다시 4방향 확인
             }
         }
-
-        // 3. 네 방향 모두 청소 완료 or 벽인 경우 → 후진 시도
-        int backDir = (dir + 2) % 4; // 현재 방향 기준으로 후진 방향 계산
+        // 3. 네 방향 모두 청소 완료 or 벽인 경우, 후진 시도
+        int backDir = (dir + 2) % 4;
         int bx = x + dx[backDir];
         int by = y + dy[backDir];
-
         // 후진할 수 없으면 종료
         if (bx < 0 || by < 0 || bx >= n || by >= m || room[bx][by] == 1) {
             return;
         }
-
         // 후진하면서 방향 유지
         clean(bx, by, dir);
     }
