4e17fabc29a2fac3a2c55210e63bbfbc71fd12d0
hanjihae <86669962+hanjihae@users.noreply.github.com>
2025-02-19 10:41:30 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -3,33 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// dp[1] != dp[2]
-// dp[n] != dp[n-1]
-// dp[i] != dp[i-1] && dp[i] != dp[i+1]
-// n개의 집을 칠하는 비용, 차례대로 빨강, 초록, 파랑
-
+// 집은 빨강, 초록, 파랑 중 하나로 칠해야만
+// 1번집 색 != 2번집 색
+// n번집 색 != n-1번집 색
+// i번집 색!= i-1번집 색, i+1번집 색, i (2 <= i <= n-1)
 public class Main {
-    static int n;
-    static int[][] budget;
-    static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        budget = new int[n][3];
-        dp = new int[n][3];
-        for (int i=0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j=0; j < 3; j++) {
-                budget[i][j] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int[][] colors = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                colors[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        dp[0][0] = budget[0][0];    // 빨강
-        dp[0][1] = budget[0][1];    // 초록
-        dp[0][2] = budget[0][2];    // 파랑
-        for (int i=1; i < n; i++) {
-            dp[i][0] = budget[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);
-            dp[i][1] = budget[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
-            dp[i][2] = budget[i][2] + Math.min(dp[i-1][0], dp[i-1][1]);
+        int[][] dp = new int[n][3];
+        dp[0][0] = colors[0][0];    // 첫번째 집을 빨강으로 칠하는 비용
+        dp[0][1] = colors[0][1];    // 초록
+        dp[0][2] = colors[0][2];    // 파랑
+        for (int i = 1; i < n; i++) {
+            // 현재 집을 빨강으로 칠하는 경우 다음집은 초록 또는 파랑
+            dp[i][0] = colors[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);
+            dp[i][1] = colors[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
+            dp[i][2] = colors[i][2] + Math.min(dp[i-1][0], dp[i-1][1]);
         }
         int min = Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2]));
         System.out.println(min);
