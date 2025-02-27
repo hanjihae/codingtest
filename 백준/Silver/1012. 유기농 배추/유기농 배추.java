85839870181af87e50e29aceee3fe2d909afdc78
hanjihae <86669962+hanjihae@users.noreply.github.com>
2025-02-27 10:45:54 +0900

    [Silver II] Title: 유기농 배추, Time: 128 ms, Memory: 15804 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,56 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+// 한 배추의 상하좌우 네 방향에 다른 배추가 위치한 경우 == 인접하다
+// 한 배추에 지렁이 있으면 인접한 배추들로 이동 가능
+// 지렁이 몇 마리 필요한지
+
 public class Main {
-    static int m, n, k;
-    static int[][] arr;
+    static int m, n;
     static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};    // 상하
-    static int[] dy = {0, 0, -1, 1};    // 좌우
+    static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        for(int i=0; i < t; i++) {
+        for (int i = 0; i < t; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            m = Integer.parseInt(st.nextToken());   // 배추밭 가로
-            n = Integer.parseInt(st.nextToken());   // 배추밭 세로
-            k = Integer.parseInt(st.nextToken());   // 배추 위치
-            arr = new int[m][n];
-            visited = new boolean[m][n];
-            for (int j=0; j < k; j++) {
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            int[][] field = new int[m][n];
+            for (int j = 0; j < k; j++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-                arr[x][y] = 1;
+                field[x][y] = 1;
             }
-            int cnt = 0;    // 필요한 배추흰지렁이 수
-            for (int a=0; a < m; a++) {
-                for (int b=0; b < n; b++) {
-                    // 배추가 심어져 있고 방문하지 않은 경우
-                    if (arr[a][b] == 1 && !visited[a][b]) {
-                        dfs(a, b);  // 인접한 영역 모두 방문처리
+            visited = new boolean[m][n];
+            int cnt = 0;
+            for (int a = 0; a < m; a++) {
+                for (int b = 0; b < n; b++) {
+                    if (field[a][b] == 1 && !visited[a][b]) {
+                        dfs(a, b, field);
                         cnt++;
-                    }
+                    }   // 1인데 방문하지 않은 곳에서 인접까지 방문하고 와서 +1
                 }
             }
             System.out.println(cnt);
         }
     }
 
-    public static void dfs(int x, int y) {
-        visited[x][y] = true;   // 방문 처리
-        // 상하좌우 이동하며 연결된 영역 찾기
-        for (int i=0; i < 4; i++) {
-            int newX = x + dx[i];
-            int newY = y + dy[i];
-            // 새로운 좌표가 배추밭 내에 있고
-            if (newX >= 0 && newY >= 0 && newX < m && newY < n) {
-                // 배추가 심어져 있고 방문하지 않은 경우
-                if (arr[newX][newY] == 1 && !visited[newX][newY]) {
-                    dfs(newX, newY);
-                }
+    public static void dfs(int x, int y, int[][] field) {
+        visited[x][y] = true;
+        for (int i=0; i<4; i++) {
+            int nx = x + dir[i][0];
+            int ny = y + dir[i][1];
+            if (nx >= 0 && ny >= 0 && nx < m && ny < n && field[nx][ny] == 1 && !visited[nx][ny]) {
+                dfs(nx, ny, field);
             }
         }
     }
